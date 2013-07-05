deb595a965941f0699bab931c7867eeddfa305e3
Tristan Seifert <tristan@squee.co>
2013-07-05 14:03:20 -0500

    Created Xcode project.

---
+++ QuickHAC/QuickHAC/SQUAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  SQUAppDelegate.h
+//  QuickHAC
+//
+//  Created by Tristan Seifert on 05/07/2013.
+//  Copyright (c) 2013 Squee! Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SQUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ QuickHAC/QuickHAC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  QuickHAC
+//
+//  Created by Tristan Seifert on 05/07/2013.
+//  Copyright (c) 2013 Squee! Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SQUAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SQUAppDelegate class]));
+    }
+}
